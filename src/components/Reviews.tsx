17ca477258212ade1767e663b0a0f3ff50fc5bcb
Djordje J <djolecs97@gmail.com>
2024-04-21 11:07:44 +0200

    refactor: clean up the code

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -26,116 +26,132 @@ export const Reviews: React.FC<ReviewsComponentProps> = ({
   closeAfterSubmit = true,
   classNameReviewsContainer = '',
   classNameContainer = '',
-  classNameReview = '',
+  classNameReview = ''
 }) => {
   const containerRef = useRef<HTMLDivElement>(null);
+  const sliceLeft = useRef(0);
+  const sliceRight = useRef(3);
   const [showNewReviewForm, setShowNewReviewForm] = useState(false);
   const [visibleReviews, setVisibleReviews] = useState<Review[]>([]);
   const [direction, setDirection] = useState<'left' | 'right'>('right');
 
-  const updateVisibleReviews = (containerWidth: number) => {
-    const sliceRight = containerWidth < 640 ? 1 : containerWidth < 1200 ? 2 : 3;
-    setVisibleReviews(reviews.slice(0, sliceRight));
-  };
-
   useEffect(() => {
-    const containerWidth = containerRef.current?.getBoundingClientRect().width || 0;
-    updateVisibleReviews(containerWidth);
-
-    const handleResize = () => {
+    const updateVisibleReviews = () => {
       const containerWidth = containerRef.current?.getBoundingClientRect().width || 0;
-      updateVisibleReviews(containerWidth);
+
+      if (containerWidth < 640) {
+        sliceRight.current = 1;
+      } else if (containerWidth < 1200) {
+        sliceRight.current = 2;
+      } else {
+        sliceRight.current = 3;
+      }
+
+      setVisibleReviews(reviews.slice(sliceLeft.current, sliceRight.current));
     };
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
+
+    sliceLeft.current = 0;
+    updateVisibleReviews();
+
+    const resizeListener = () => {
+      sliceLeft.current = 0;
+      updateVisibleReviews();
+    };
+
+    window.addEventListener('resize', resizeListener);
+    return () => window.removeEventListener('resize', resizeListener);
   }, [reviews]);
 
-  const handleNavigate = (direction: 'left' | 'right') => {
-    if (reviews.length === 0) return;
-
-    const currentIndex = reviews.findIndex((review) => review.id === visibleReviews[0].id);
-    const newIndex =
-      direction === 'left'
-        ? Math.max(0, currentIndex - visibleReviews.length)
-        : Math.min(reviews.length - visibleReviews.length, currentIndex + visibleReviews.length);
-    setVisibleReviews(reviews.slice(newIndex, newIndex + visibleReviews.length));
-    setDirection(direction);
+  const handlePrevClick = () => {
+    const currentRange = sliceRight.current - sliceLeft.current;
+    sliceLeft.current = Math.max(0, sliceLeft.current - currentRange);
+    sliceRight.current = Math.max(currentRange, sliceRight.current - currentRange);
+    setVisibleReviews(reviews.slice(sliceLeft.current, sliceRight.current));
+    setDirection('left');
   };
 
-  const averageRating = reviews.length ? reviews.reduce((sum, review) => sum + review.rating, 0) / reviews.length : 0;
+  const handleNextClick = () => {
+    const currentRange = sliceRight.current - sliceLeft.current;
+    if (sliceLeft.current + currentRange >= reviews.length) return;
+    sliceLeft.current += currentRange;
+    sliceRight.current += currentRange;
+    setVisibleReviews(reviews.slice(sliceLeft.current, sliceRight.current));
+    setDirection('right');
+  };
+
+  const averageRating = reviews.length ? reviews.reduce((acc, review) => acc + review.rating, 0) / reviews.length : 0;
 
   return (
     <div ref={containerRef} className={`text-white relative w-full ml-auto mr-auto ${classNameContainer}`}>
       <h1 className="text-2xl mb-2 font-bold text-center">Customer reviews</h1>
 
       {reviews.length > 0 && (
-        <>
-          <div className="flex justify-center items-center gap-2 mb-5">
-            <div className="flex gap-1">
-              {[...Array(Math.floor(averageRating))].map((_, index) => (
-                <img key={index} src={pathToIcon || Star} alt="star" className="max-w-[30px]" />
-              ))}
-            </div>
-            <span>{averageRating.toFixed(1)}</span>
-          </div>
-
-          <TransitionGroup
-            className={`flex gap-3 w-full justify-center items-center min-h-[350px] ${classNameReviewsContainer}`}
-            style={{
-              '--enter-transform': direction === 'left' ? '-50%' : '50%',
-              '--exit-transform': direction === 'left' ? '50%' : '-50%',
-            } as React.CSSProperties}
-          >
-            {visibleReviews.map((review) => (
-              <CSSTransition key={review.id} timeout={700} classNames="review">
-                {_ReviewComponent ? (
-                  <_ReviewComponent review={review} />
-                ) : (
-                  <ReviewComponent review={review} pathToIcon={pathToIcon} className={classNameReview} />
-                )}
-              </CSSTransition>
+        <div className="flex justify-center items-center gap-2 mb-5">
+          <div className="flex gap-1">
+            {[...Array(Math.floor(averageRating))].map((_, index) => (
+              <img key={index} src={pathToIcon || Star} alt="star" className="max-w-[30px]" />
             ))}
-          </TransitionGroup>
+          </div>
+          <span>{averageRating.toFixed(1)}</span>
+        </div>
+      )}
 
-          <div className="mt-3 flex justify-center">
-            <button
-              className="reviews-btn text-white font-bold rounded mr-3 bg-main-light p-3"
-              onClick={() => handleNavigate('left')}
-              disabled={reviews.length === 0 || visibleReviews[0]?.id === reviews[0]?.id}
-            >
+      {reviews.length > 0 ? (
+        <TransitionGroup
+          className={`flex gap-3 w-full justify-center items-center min-h-[350px] ${classNameReviewsContainer}`}
+          style={{
+            '--enter-transform': direction === 'left' ? '-50%' : '50%',
+            '--exit-transform': direction === 'left' ? '50%' : '-50%',
+          } as React.CSSProperties}
+        >
+          {visibleReviews.map((review) => (
+            <CSSTransition key={review.id} timeout={700} classNames="review">
+              {_ReviewComponent ? (
+                <_ReviewComponent review={review} />
+              ) : (
+                <ReviewComponent review={review} pathToIcon={pathToIcon} className={classNameReview} />
+              )}
+            </CSSTransition>
+          ))}
+        </TransitionGroup>
+      ) : (
+        <div className="flex justify-center items-center w-full h-full">
+          <h1 className="text-2xl">No reviews yet.</h1>
+        </div>
+      )}
+
+      <div className="mt-3 flex justify-center">
+        {reviews.length > 0 && (
+          <>
+            <button className="text-white font-bold rounded mr-3 bg-main-light p-3" onClick={handlePrevClick}>
               <img src={Left} alt="left" className="max-w-[30px] w-[30px]" />
             </button>
-            <button
-              className="reviews-btn text-white font-bold rounded bg-main-light p-3"
-              onClick={() => handleNavigate('right')}
-              disabled={reviews.length === 0 || visibleReviews[visibleReviews.length - 1]?.id === reviews[reviews.length - 1]?.id}
-            >
+            <button className="text-white font-bold rounded bg-main-light p-3" onClick={handleNextClick}>
               <img src={Right} alt="right" className="max-w-[30px] w-[30px]" />
             </button>
-          </div>
-        </>
-      )}
-
-      {reviews.length === 0 && <p className="text-center text-xl">No reviews yet.</p>}
-
-      {onSubmit && (
-        <button
-          className="text-white font-bold rounded bg-main-light p-3 mt-3 mx-auto block"
-          onClick={() => setShowNewReviewForm(!showNewReviewForm)}
-        >
-          {showNewReviewForm ? 'Close' : 'Add Review'}
-        </button>
-      )}
+          </>
+        )}
+        {onSubmit && (
+          <button
+            className="text-white font-bold rounded bg-main-light p-3 ml-3 mt-0"
+            onClick={() => setShowNewReviewForm(!showNewReviewForm)}
+          >
+            {showNewReviewForm ? 'Close' : 'Add Review'}
+          </button>
+        )}
+      </div>
 
       {showNewReviewForm && onSubmit && (
-        <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center z-50">
-          <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50" onClick={() => setShowNewReviewForm(false)} />
+        <div className="top-0 left-0 w-full h-full fixed flex justify-center items-center z-[5]">
+          <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50" onClick={() => setShowNewReviewForm(false)}></div>
           <ReviewForm
             onSubmit={(review) => {
               onSubmit(review);
-              closeAfterSubmit && setShowNewReviewForm(false);
+              if (closeAfterSubmit) {
+                setShowNewReviewForm(false);
+              }
             }}
-            className="bg-slate-900 relative z-10 p-3 max-w-[500px] w-full"
+            className="bg-slate-900 relative z-[5] p-3 max-w-[500px] w-full"
           />
         </div>
       )}
